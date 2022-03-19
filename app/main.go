5f46b2a0cd3fb7556a3eab71cfc653a465624f75
Gary Morse <gmorse81@gmail.com>
2022-03-19 12:22:59 -0400

    part 1 basic Go web server

---
+++ app/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func newRouter() *httprouter.Router {
+	mux := httprouter.New()
+
+	mux.GET("/youtube/channel/stats", getChannelStats())
+
+	return mux
+}
+
+func getChannelStats() httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		w.Write([]byte("response!"))
+	}
+}
+
+func main() {
+
+	srv := &http.Server{
+		Addr:    ":10101",
+		Handler: newRouter(),
+	}
+
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		signal.Notify(sigint, syscall.SIGTERM)
+		<-sigint
+
+		log.Println("service interrupt received")
+
+		log.Println("http server shutting down")
+		time.Sleep(5 * time.Second)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Printf("http server shutdown error: %v", err)
+		}
+
+		log.Println("shutdown complete")
+
+		close(idleConnsClosed)
+
+	}()
+
+	log.Printf("Starting server on port 10101")
+	if err := srv.ListenAndServe(); err != nil {
+		if !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("fatal http server failed to start: %v", err)
+		}
+	}
+
+	<-idleConnsClosed
+	log.Println("Service Stop")
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/askcloudarchitech/go-rest-api-kubernetes-example
+
+go 1.17
+
+require github.com/julienschmidt/httprouter v1.3.0
